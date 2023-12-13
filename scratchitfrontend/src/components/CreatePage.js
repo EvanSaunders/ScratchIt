808a207a6940d0a47407c297f5259660fe5bb795
EvanSaunders <evansaund@gmail.com>
2023-12-13 16:54:29 -0600

    Started work on User Authentication
    Sign in works, TODO use decoder

--- scratchitfrontend/src/components/CreatePage.js
+++ scratchitfrontend/src/components/CreatePage.js
@@ -4,15 +4,11 @@ import UserFields from "./UserFields";
 import { useAuth } from './AuthContext';
 
 function AuthRequiredComponent() {
-    const { isAuthenticated } = useAuth();
 
-    if (!isAuthenticated) {
-        // Redirect or show login component if not authenticated
-        return <p>Please log in to access this content.</p>;
-    }
 
     return (
         <div>
+
             <UserFields/>
         </div>
     );
--- scratchitfrontend/src/components/Home.js
+++ scratchitfrontend/src/components/Home.js
@@ -9,30 +9,7 @@ const clientId = "282650218440-6vb2j84rru32vf15iuli4bddqdpkq61l.apps.googleuserc
 function Home() {
     const navigate = useNavigate();
 
-    useEffect(() => {
-        function loadGoogleApi() {
-            gapi.load('client', {
-                callback: initializeGoogleApi,
-                onerror: () => console.error('Failed to load Google API client'),
-                timeout: 10000, // Set a timeout to handle potential loading issues
-                ontimeout: () => console.error('Timeout loading Google API client'),
-            });
-        }
 
-        function initializeGoogleApi() {
-            gapi.client.init({
-                clientId: clientId,
-                scope: ""
-            }).then(() => {
-                console.log('Google API client initialized successfully');
-            }).catch((error) => {
-                console.error('Error initializing Google API client', error);
-            });
-        }
-
-        // Call the loadGoogleApi function to load and initialize the Google API client
-        loadGoogleApi();
-    }, []); // Empty dependency array means this effect runs once when the component mounts
 
     const handleNavigation = () => {
         // Correct way to navigate without full page reload
@@ -40,11 +17,14 @@ function Home() {
     };
 
     return (
+
         <div className="Home">
+
             <Appbar />
-            <Link to="/create-cards">Link to Create Cards</Link>
+            <div id="signInDiv"></div>
+
             <button onClick={handleNavigation}>Navigate to Create Cards</button>
-            <Login />
+
         </div>
     );
 }
